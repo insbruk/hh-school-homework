7c202dcb936e7c406a9316fe4c048d4c024afaef
mumok4 <nosachenkoalbert@gmail.com>
2026-03-31 14:55:26 +0800

    Added homework task

--- public/index.js
+++ public/index.js
@@ -4,26 +4,91 @@ import * as view from './view.js';
 
 // Задача #1
 async function handleLogin () {
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({username:'albert', password:'A.nosach'})
+    })
+
+    if (!response.ok) throw new Error('Login failed.');
+
+    const data = await response.json();
+    localStorage.setItem('accessToken', data.accessToken);
+
     console.log('login')
 }
 
 // Задача #2
 async function handleInputChange({ target: { value }}) {
-    console.log(`searchTitle changed to "${value}"`)
+    store.setSearchTitle(value);
+    console.log(`searchTitle changed to "${value}"`);
 }
 
 // Задача #2
 async function handleSelectChange({ target: { value }}) {
-    console.log(`filterStatus changed to "${value}"`)
+    store.setFilterStatus(value);
+    console.log(`filterStatus changed to "${value}"`);
 }
 
 // Задача #3 и #4
 async function handleSearchTasks() {
+    const token = localStorage.getItem('accessToken');
+    if (!token) return;
+
+    const searchTitle = localStorage.getItem('searchTitle') || '';
+    const filterStatus = localStorage.getItem('filterStatus') || '';
+
+    navigator.sendBeacon(
+      '/analytics',
+      new Blob(
+        [JSON.stringify({ action: 'search', searchTitle, filterStatus }),],
+        { type: 'application/json' }
+      )
+    )
+
+    const params = new URLSearchParams();
+    if (searchTitle) params.set('title', searchTitle);
+    if (filterStatus) params.set('status', filterStatus);
+
+    const url = `/tasks${params.toString() ? '?' + params.toString() : ''}`;
+
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    if (response.status === 401) {
+        localStorage.removeItem('accessToken');
+        return;
+    }
+
+    if (!response.ok) throw new Error('Problem fetching tasks');
+
+    const data = await response.json();
+
+    store.setTasks(JSON.stringify(data.items));
+
     console.log('search')
 }
 
 // Задача #5
 async function handleLogout () {
+    const token = localStorage.getItem('accessToken');
+
+    const response = await fetch('/logout', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+        },
+    });
+
+    if (!response.ok) throw new Error('Logout failed');
+
+    localStorage.removeItem('accessToken');
+
     console.log('logout')
 }
 
